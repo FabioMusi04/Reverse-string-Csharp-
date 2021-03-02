f9d6be6571ef4dfe8e7d4a68d167d0462ea348ef
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2021-03-02 10:49:49 +0100

    Add files via upload

---
+++ c# Reverse text vscode/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace c__Reverse_text_vscode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            inizio:
+            Console.Write("Inserisci la parola da ribaltare:   ");
+            string Parola = Console.ReadLine();
+            Console.WriteLine($"Ecco la parola ribaltata: {Reversa(Parola)}");
+            Console.WriteLine("-----------===================-----------");
+            Console.WriteLine("Premi R per Continuare o un qualsiasi altro tasto per uscire");
+            if(Console.ReadKey(true).Key == ConsoleKey.R)
+            {
+                Console.Clear();
+                goto inizio;
+            }
+            else
+            {
+                Console.WriteLine("ADDIO");
+                Environment.Exit(0);
+            }
+        }
+        public static string Reversa(string Parola)
+        {
+            if(Parola == null) return null;
+            char[] chars = Parola.ToCharArray();
+            Array.Reverse(chars);
+            return new String(chars);   
+        }
+    }
+}
